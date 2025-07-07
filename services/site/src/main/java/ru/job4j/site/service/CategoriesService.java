e33183f963871d25d93bc06a4a09e0d2fd3b96a3
Admin <mishavolgodonsk@gmail.com>
2025-07-07 19:06:59 +0300

    RestTemplate [#458496]

--- services/site/src/main/java/ru/job4j/site/service/CategoriesService.java
+++ services/site/src/main/java/ru/job4j/site/service/CategoriesService.java
@@ -6,13 +6,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.job4j.site.dto.CategoryDTO;
+import ru.job4j.site.dto.InterviewDTO;
+import ru.job4j.site.dto.TopicDTO;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
 public class CategoriesService {
     private final TopicsService topicsService;
+
+    private final InterviewsService interviewsService;
 
     public List<CategoryDTO> getAll() throws JsonProcessingException {
         var text = new RestAuthCall("http://localhost:9902/categories/").get();
@@ -61,8 +67,18 @@ public class CategoriesService {
 
     public List<CategoryDTO> getMostPopular() throws JsonProcessingException {
         var categoriesDTO = getPopularFromDesc();
+        Map<Integer, Long> interviewCountsByTopic = interviewsService.getByType(1).stream()
+                .collect(Collectors.groupingBy(
+                        InterviewDTO::getTopicId,
+                        Collectors.counting()
+                ));
         for (var categoryDTO : categoriesDTO) {
-            categoryDTO.setTopicsSize(topicsService.getByCategory(categoryDTO.getId()).size());
+            List<TopicDTO> categoryTopics = topicsService.getByCategory(categoryDTO.getId());
+            categoryDTO.setTopicsSize(categoryTopics.size());
+            long totalCount = categoryTopics.stream()
+                    .mapToLong(topic -> interviewCountsByTopic.getOrDefault(topic.getId(), 0L))
+                    .sum();
+            categoryDTO.setNewInterviewsCount((int) totalCount);
         }
         return categoriesDTO;
     }
